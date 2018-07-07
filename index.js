1b83465205dd5463a5f666263e8f44c092361c44
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-07-07 14:01:57 +0200

    Export core in index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 const Core = require("./src/core.class");
 
-async function main() {
-    const agent = new Core();
-    await agent.initialize();
-}
-main().catch(console.error);
+module.exports = Core;
